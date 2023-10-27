5264198e0154e1a6bb1a71d5a3aebdac493f0e5d
Philipp Ifland <83982559+philippifland@users.noreply.github.com>
2023-10-27 10:57:14 +0000

    sine animation works

--- pisinus/script.js
+++ pisinus/script.js
@@ -8,34 +8,34 @@ var arrlen = 500;
 for(var i = 0; i < arrlen; i++){
     divs.push(document.createElement('div'));
     divs[i].classList.add('sine-element');
-    var iteration = i/arrlen * 2 * Math.PI;
-    divs[i].style.top = 250 + 250 * Math.sin(iteration) + 'px';
+    var position = i/arrlen * 2 * Math.PI;
+    divs[i].style.top = 250 + 250 * Math.sin(position) + 'px';
     sineContainer.appendChild(divs[i]);
 }
 
 console.log(divs)
 
 function moveStuff(){
-    moveBar();
-    moveDots();
+    count();
+    moveBar(2*Math.PI*position/arrlen);
+    moveDots(position);
 }
 
-var start = 0;
-function moveBar(){
-    bar.style.top = 245 - 245 * Math.sin( start ) + 'px';
-    bar.style.left = -245 * Math.cos( start ) + 245 + 'px';
-    bar.style.width = 245 + 245 * Math.cos(start) + 'px';
-    start += Math.PI/100/2.5;
+var position = 0;
+function count(){
+    position+=0.6;
+    if(position>arrlen) position = 0;
 }
 
-var start2 = 0;
-function moveDots(){
+function moveBar(y){
+    bar.style.top = 250 - 250 * Math.sin( y ) + 'px';
+    bar.style.left = -250 * Math.cos( y ) + 245 + 'px';
+    bar.style.width = 100 + 250 + 250 * Math.cos(y) + 'px';
+}
+
+function moveDots(x){
     for(var i=0; i<arrlen; i++){
-        divs[i].style.left = (i+125*start)%arrlen+'px';
-    }
-    start2++;
-    if(start2>=arrlen){
-        start2=0;
+        divs[i].style.left = (i+x)%(arrlen)+'px';
     }
 }
-window.setInterval(moveStuff, 10);
+window.setInterval(moveStuff, 5);
